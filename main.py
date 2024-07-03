52fbe1e0e521b05804d453fbd38365f8cb8c7d9f
johngeorgiades <johndgeorgiades@gmail.com>
2024-07-03 12:37:49 -0500

    This is what the guy has in his code up to here.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # importing modules
 from Bio.PDB.PDBParser import PDBParser
+from Bio.PDB.Polypeptide import three_to_one
+from Bio.PDB.Structure import Structure
 from Bio.SVDSuperimposer import SVDSuperimposer
 import numpy as np
 
@@ -11,3 +13,12 @@ model = p.get_structure("model", "1ht0.pdb")
 chain_a = native[0]['A']
 residues_a = [r for r in chain_a]
 
+AA = ["ALA", "CYS", "ASP", "GLU", "PHE", "GLY", "HIS", "ILE", "LYS", "LEU", "MET", "ASN", "PRO", "GLN",
+      "ARG", "SER", "THR", "VAL", "TRP", "TYR"]
+
+def align(native: Structure, model:Structure, atom_types = ["CA", "N", "C", "O"]) -> SVDSuperimposer:
+      """Alignts a model structure onto a native structure using the atom types listed in 'atom_types'."""
+
+      # A long one-liner that gets the one-letter amino acid representation for each residue in a structure,
+      # then joins those letters into one long string.
+      native_seq = "".join([three_to_one(r.resname) for r in native[0].get_residues() if r.resname in AA])
