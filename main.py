2e049d554eb5aebc08144790de85c09d09bfd640
johngeorgiades <johndgeorgiades@gmail.com>
2024-07-09 10:02:41 -0500

    Added hierarchical clustering from a pre-calculated distance matrix via biopython.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.Polypeptide import is_aa
 from Bio.PDB.Structure import Structure
 from Bio.SVDSuperimposer import SVDSuperimposer
+from Bio import Cluster
 
 ####################################
 # Import structures file as an array
@@ -140,3 +141,10 @@ np.savetxt(fname="distance_matrix", X=distance_matrix, delimiter=",")
 # Hierarchical Clustering from Distance Matrix
 ##############################################
 
+# Cluster.treecluster() may shuffle data in the distance matrix, so it will be fed a duplicate.
+
+distance_matrix_duplicate = distance_matrix
+
+globalRMSDTree = Cluster.treecluster(data=None, distancematrix=distance_matrix_duplicate, method="a")  # unsorted
+
+print(globalRMSDTree)
